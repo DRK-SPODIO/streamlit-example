b89db2aa976ee0613050c661a1d5a7d43702112a
DRK <rkerr@spodiogroup.com>
2022-04-20 13:42:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,10 +49,10 @@ styles = [
     dict(selector="caption", props=[("caption-side", "bottom")])
 ]
 
-Display_df.style.set_table_styles(styles)    
-Display_df['link'] = ['<a href="'+ str(x) +'" target = "_blank">Link to Post</a>' for x in Display_df['link'].tolist()]
+Display_df['link'] = ['<a href="'+ str(x) +'" target = "_blank">Link to Post</a>' if not pd.isna(x) else 'No Link' for x in  Display_df['link'].tolist()]
 Display_df['summary'] = Display_df['summary'].fillna('No Summary') 
+Display_df.style.set_table_styles(styles)
 
 
 # In[]
-components.html(Display_df.style.to_html(),width=1200, height=800, scrolling=True)
+st.markdown(Display_df.style.to_html(),width=1200, height=800, scrolling=True)
