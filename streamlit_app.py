39ded42164ae5bd2c7e5f4a2aa9c482ee25dffb0
DRK <rkerr@spodiogroup.com>
2022-04-20 13:47:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,4 +56,4 @@ Disp = Display_df.style.set_table_styles(styles)
 
 # In[]
 # components.html(Display_df.style.to_html(),width=1200, height=800, scrolling=True)
-st.dataframe(data=Disp,width=1200, height=800, scrolling=True)
+st.dataframe(data=Disp,width=1400, height=1000)
