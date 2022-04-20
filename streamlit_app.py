dec147d50edba9aa94bd6f5ef8cf59a6097b1855
DRK <rkerr@spodiogroup.com>
2022-04-20 11:14:29 -0700

    Adding Post df and data assetsq
    + Post Excel file
    + RSS Links Excel file
    
    test of DF display on StreamLit

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,7 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+
+Post_df = pd.read_excel('Post_History.xlsx')
+Post_df
