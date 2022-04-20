715ee6900ce8d0e7d771efbcc32179329307dd3b
DRK <rkerr@spodiogroup.com>
2022-04-20 13:43:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,4 +55,5 @@ Display_df.style.set_table_styles(styles)
 
 
 # In[]
-st.markdown(Display_df.style.to_html(),width=1200, height=800, scrolling=True)
+# components.html(Display_df.style.to_html(),width=1200, height=800, scrolling=True)
+st.markdown(Display_df.style.to_html())
