aa9457a56e18c7676750dc606fcf692e06388f09
DRK <rkerr@spodiogroup.com>
2022-05-05 13:04:39 -0700

    Post Uodate & LDA Anaalysis

--- RSS Feed Data Update.py
+++ RSS Feed Data Update.py
@@ -39,7 +39,7 @@ for feed in feeds:
     Post_Counts.append(len(feed[2]))
     Feeds_DF = Feeds_DF.append(Feed_DF)
 
-# In[]
+
 df = Feeds_DF[['published', 'title', 'link', 'summary', 'author']].copy() # pass data to init
 
 # Fix dates and make Universal time
@@ -66,14 +66,8 @@ Post_df['link'] = [x if len(x) > 1 else 'No Link' for x in Post_df['link'].tolis
 Post_df['author'] = [x if len(x) > 1 else 'No Author' for x in Post_df['author'].tolist()]
 
 
-# In[]
-Post_df = Post_df.drop_duplicates(subset=['title', 'link', 'summary'], keep='last').copy()
-# In[]
+Post_df = Post_df.drop_duplicates(subset=['title', 'link', 'summary'], keep='first').copy()
 
-
-
-
-# In[]
 # Write to excel File
 print('Saving to Excel file...')
 Post_df.to_excel('Post_History.xlsx', index=False)
@@ -81,6 +75,7 @@ print('Data update Complete')
 
 # In[]
 
+print('Processing Post data...')
 # Regular Expression to remove non-printable type
 CLEANR = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
 
@@ -88,7 +83,7 @@ CLEANR = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
 STOPWORDS = ["0o", "0s", "3a", "3b", "3d", "6b", "6o", "a", "a1", "a2", "a3", "a4", "ab", "able", "about", "above", "abst", "ac", "accordance", "according", "accordingly", "across", "act", "actually", "ad", "added", "adj", "ae", "af", "affected", "affecting", "affects", "after", "afterwards", "ag", "again", "against", "ah", "ain", "ain't", "aj", "al", "all", "allow", "allows", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "announce", "another", "any", "anybody", "anyhow", "anymore", "anyone", "anything", "anyway", "anyways", "anywhere", "ao", "ap", "apart", "apparently", "appear", "appreciate", "appropriate", "approximately", "ar", "are", "aren", "arent", "aren't", "arise", "around", "as", "a's", "aside", "ask", "asking", "associated", "at", "au", "auth", "av", "available", "aw", "away", "awfully", "ax", "ay", "az", "b", "b1", "b2", "b3", "ba", "back", "bc", "bd", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "begin", "beginning", "beginnings", "begins", "behind", "being", "believe", "below", "beside", "besides", "best", "better", "between", "beyond", "bi", "bill", "biol", "bj", "bk", "bl", "bn", "both", "bottom", "bp", "br", "brief", "briefly", "bs", "bt", "bu", "but", "bx", "by", "c", "c1", "c2", "c3", "ca", "call", "came", "can", "cannot", "cant", "can't", "cause", "causes", "cc", "cd", "ce", "certain", "certainly", "cf", "cg", "ch", "changes", "ci", "cit", "cj", "cl", "clearly", "cm", "c'mon", "cn", "co", "com", "come", "comes", "con", "concerning", "consequently", "consider", "considering", "contain", "containing", "contains", "corresponding", "could", "couldn", "couldnt", "couldn't", "course", "cp", "cq", "cr", "cry", "cs", "c's", "ct", "cu", "currently", "cv", "cx", "cy", "cz", "d", "d2", "da", "date", "dc", "dd", "de", "definitely", "describe", "described", "despite", "detail", "df", "di", "did", "didn", "didn't", "different", "dj", "dk", "dl", "do", "does", "doesn", "doesn't", "doing", "don", "done", "don't", "down", "downwards", "dp", "dr", "ds", "dt", "du", "due", "during", "dx", "dy", "e", "e2", "e3", "ea", "each", "ec", "ed", "edu", "ee", "ef", "effect", "eg", "ei", "eight", "eighty", "either", "ej", "el", "eleven", "else", "elsewhere", "em", "empty", "en", "end", "ending", "enough", "entirely", "eo", "ep", "eq", "er", "es", "especially", "est", "et", "et-al", "etc", "eu", "ev", "even", "ever", "every", "everybody", "everyone", "everything", "everywhere", "ex", "exactly", "example", "except", "ey", "f", "f2", "fa", "far", "fc", "few", "ff", "fi", "fifteen", "fifth", "fify", "fill", "find", "fire", "first", "five", "fix", "fj", "fl", "fn", "fo", "followed", "following", "follows", "for", "former", "formerly", "forth", "forty", "found", "four", "fr", "from", "front", "fs", "ft", "fu", "full", "further", "furthermore", "fy", "g", "ga", "gave", "ge", "get", "gets", "getting", "gi", "give", "given", "gives", "giving", "gj", "gl", "go", "goes", "going", "gone", "got", "gotten", "gr", "greetings", "gs", "gy", "h", "h2", "h3", "had", "hadn", "hadn't", "happens", "hardly", "has", "hasn", "hasnt", "hasn't", "have", "haven", "haven't", "having", "he", "hed", "he'd", "he'll", "hello", "help", "hence", "her", "here", "hereafter", "hereby", "herein", "heres", "here's", "hereupon", "hers", "herself", "hes", "he's", "hh", "hi", "hid", "him", "himself", "his", "hither", "hj", "ho", "home", "hopefully", "how", "howbeit", "however", "how's", "hr", "hs", "http", "hu", "hundred", "hy", "i", "i2", "i3", "i4", "i6", "i7", "i8", "ia", "ib", "ibid", "ic", "id", "i'd", "ie", "if", "ig", "ignored", "ih", "ii", "ij", "il", "i'll", "im", "i'm", "immediate", "immediately", "importance", "important", "in", "inasmuch", "inc", "indeed", "index", "indicate", "indicated", "indicates", "information", "inner", "insofar", "instead", "interest", "into", "invention", "inward", "io", "ip", "iq", "ir", "is", "isn", "isn't", "it", "itd", "it'd", "it'll", "its", "it's", "itself", "iv", "i've", "ix", "iy", "iz", "j", "jj", "jr", "js", "jt", "ju", "just", "k", "ke", "keep", "keeps", "kept", "kg", "kj", "km", "know", "known", "knows", "ko", "l", "l2", "la", "largely", "last", "lately", "later", "latter", "latterly", "lb", "lc", "le", "least", "les", "less", "lest", "let", "lets", "let's", "lf", "like", "liked", "likely", "line", "little", "lj", "ll", "ll", "ln", "lo", "look", "looking", "looks", "los", "lr", "ls", "lt", "ltd", "m", "m2", "ma", "made", "mainly", "make", "makes", "many", "may", "maybe", "me", "mean", "means", "meantime", "meanwhile", "merely", "mg", "might", "mightn", "mightn't", "mill", "million", "mine", "miss", "ml", "mn", "mo", "more", "moreover", "most", "mostly", "move", "mr", "mrs", "ms", "mt", "mu", "much", "mug", "must", "mustn", "mustn't", "my", "myself", "n", "n2", "na", "name", "namely", "nay", "nc", "nd", "ne", "near", "nearly", "necessarily", "necessary", "need", "needn", "needn't", "needs", "neither", "never", "nevertheless", "new", "next", "ng", "ni", "nine", "ninety", "nj", "nl", "nn", "no", "nobody", "non", "none", "nonetheless", "noone", "nor", "normally", "nos", "not", "noted", "nothing", "novel", "now", "nowhere", "nr", "ns", "nt", "ny", "o", "oa", "ob", "obtain", "obtained", "obviously", "oc", "od", "of", "off", "often", "og", "oh", "oi", "oj", "ok", "okay", "ol", "old", "om", "omitted", "on", "once", "one", "ones", "only", "onto", "oo", "op", "oq", "or", "ord", "os", "ot", "other", "others", "otherwise", "ou", "ought", "our", "ours", "ourselves", "out", "outside", "over", "overall", "ow", "owing", "own", "ox", "oz", "p", "p1", "p2", "p3", "page", "pagecount", "pages", "par", "part", "particular", "particularly", "pas", "past", "pc", "pd", "pe", "per", "perhaps", "pf", "ph", "pi", "pj", "pk", "pl", "placed", "please", "plus", "pm", "pn", "po", "poorly", "possible", "possibly", "potentially", "pp", "pq", "pr", "predominantly", "present", "presumably", "previously", "primarily", "probably", "promptly", "proud", "provides", "ps", "pt", "pu", "put", "py", "q", "qj", "qu", "que", "quickly", "quite", "qv", "r", "r2", "ra", "ran", "rather", "rc", "rd", "re", "readily", "really", "reasonably", "recent", "recently", "ref", "refs", "regarding", "regardless", "regards", "related", "relatively", "research", "research-articl", "respectively", "resulted", "resulting", "results", "rf", "rh", "ri", "right", "rj", "rl", "rm", "rn", "ro", "rq", "rr", "rs", "rt", "ru", "run", "rv", "ry", "s", "s2", "sa", "said", "same", "saw", "say", "saying", "says", "sc", "sd", "se", "sec", "second", "secondly", "section", "see", "seeing", "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent", "serious", "seriously", "seven", "several", "sf", "shall", "shan", "shan't", "she", "shed", "she'd", "she'll", "shes", "she's", "should", "shouldn", "shouldn't", "should've", "show", "showed", "shown", "showns", "shows", "si", "side", "significant", "significantly", "similar", "similarly", "since", "sincere", "six", "sixty", "sj", "sl", "slightly", "sm", "sn", "so", "some", "somebody", "somehow", "someone", "somethan", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "sp", "specifically", "specified", "specify", "specifying", "sq", "sr", "ss", "st", "still", "stop", "strongly", "sub", "substantially", "successfully", "such", "sufficiently", "suggest", "sup", "sure", "sy", "system", "sz", "t", "t1", "t2", "t3", "take", "taken", "taking", "tb", "tc", "td", "te", "tell", "ten", "tends", "tf", "th", "than", "thank", "thanks", "thanx", "that", "that'll", "thats", "that's", "that've", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "thered", "therefore", "therein", "there'll", "thereof", "therere", "theres", "there's", "thereto", "thereupon", "there've", "these", "they", "theyd", "they'd", "they'll", "theyre", "they're", "they've", "thickv", "thin", "think", "third", "this", "thorough", "thoroughly", "those", "thou", "though", "thoughh", "thousand", "three", "throug", "through", "throughout", "thru", "thus", "ti", "til", "tip", "tj", "tl", "tm", "tn", "to", "together", "too", "took", "top", "toward", "towards", "tp", "tq", "tr", "tried", "tries", "truly", "try", "trying", "ts", "t's", "tt", "tv", "twelve", "twenty", "twice", "two", "tx", "u", "u201d", "ue", "ui", "uj", "uk", "um", "un", "under", "unfortunately", "unless", "unlike", "unlikely", "until", "unto", "uo", "up", "upon", "ups", "ur", "us", "use", "used", "useful", "usefully", "usefulness", "uses", "using", "usually", "ut", "v", "va", "value", "various", "vd", "ve", "ve", "very", "via", "viz", "vj", "vo", "vol", "vols", "volumtype", "vq", "vs", "vt", "vu", "w", "wa", "want", "wants", "was", "wasn", "wasnt", "wasn't", "way", "we", "wed", "we'd", "welcome", "well", "we'll", "well-b", "went", "were", "we're", "weren", "werent", "weren't", "we've", "what", "whatever", "what'll", "whats", "what's", "when", "whence", "whenever", "when's", "where", "whereafter", "whereas", "whereby", "wherein", "wheres", "where's", "whereupon", "wherever", "whether", "which", "while", "whim", "whither", "who", "whod", "whoever", "whole", "who'll", "whom", "whomever", "whos", "who's", "whose", "why", "why's", "wi", "widely", "will", "willing", "wish", "with", "within", "without", "wo", "won", "wonder", "wont", "won't", "words", "world", "would", "wouldn", "wouldnt", "wouldn't", "www", "x", "x1", "x2", "x3", "xf", "xi", "xj", "xk", "xl", "xn", "xo", "xs", "xt", "xv", "xx", "y", "y2", "yes", "yet", "yj", "yl", "you", "youd", "you'd", "you'll", "your", "youre", "you're", "yours", "yourself", "yourselves", "you've", "yr", "ys", "yt", "z", "zero", "zi", "zz"]
 STOPWORDS.extend(['nan','sports','post','continue','reading', 'appeared', 'saturday',
                   'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
-                  'appeared', 'sportsnetca', 'read', 'sportsnet', '2022', '22bet'])
+                  'appeared', 'sportsnetca', 'read', 'sportsnet', '2022', '22bet', 'summary'])
 
 
 def cleanhtml(raw_html):
@@ -112,7 +107,7 @@ CleanTitle = [" ".join(y.lower() for y in x.split()) for x in CleanTitle]  # cle
 
 Combined = []
 for (Summary, Title) in zip(CleanDisc, CleanTitle):
-    Combined.append(Summary + Title)
+    Combined.append(Summary + ' ' + Title)
 
 CleanDisc = Combined
 #  Collect Summaries by Date
@@ -142,11 +137,37 @@ find the count of each token for the day.  We then map these counts to tokens fo
 The sum of token counts (Ex stopwords) may be a proxy for the posts that are driving Keyword
 counts."""
 
+# L
+# # Download Wordnet through NLTK in python console:
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+
+# Init the Wordnet Lemmatizer
+lemmatizer = WordNetLemmatizer()
+
+def Wordnet_pos(word):
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
+
 # Calculate Daily Token Counts (Ex Stopwords)
 CleanDisc_Daily = CleanDisc_df.groupby(['Date'], as_index=False).agg({'Summary': ' '.join})
 CleanDisc_Daily['Tokens'] = [x.split() for x in CleanDisc_Daily['Summary'].tolist()]
 CleanDisc_Daily['Token_Values'] = [list(set(x)) for x in CleanDisc_Daily['Tokens'].tolist()]
 CleanDisc_Daily['Token_Values'] = [[x for x in y if x not in STOPWORDS] for y in CleanDisc_Daily['Token_Values'].tolist()]
+
+CleanDisc_Daily['Token_Values'] = [[lemmatizer.lemmatize(token, Wordnet_pos(token)) for token in post_text] for post_text in CleanDisc_Daily['Token_Values'].tolist()]
+
+
 CleanDisc_Daily['Token_Counts'] = [[Tokens.count(x) if Tokens.count(x) > 1 else 0 for x in Token_list] for (Token_list, Tokens) in zip(CleanDisc_Daily['Token_Values'].tolist(), CleanDisc_Daily['Tokens'].tolist())]
 
 # Normalize raw counts
@@ -208,6 +229,102 @@ CleanDisc_df['Token_Score_Aged'].plot.kde()
 
 CleanDisc_Final.to_excel('Post_Analytics.xlsx')
 
+# In[]
+papers = pd.DataFrame()
+papers['title'] = CleanDisc_Final['Title']
+papers['paper_text'] = CleanDisc_Final['Post Text']
+
+# Remove punctuation
+
+papers['paper_text_processed'] =[cleanhtml(str(post)) for post in papers['paper_text']]
+
+papers['paper_text_processed'] = papers['paper_text_processed'].map(lambda x: re.sub('[,\.!?]', '', x))
+# Convert the titles to lowercase
+papers['paper_text_processed'] = papers['paper_text_processed'].map(lambda x: x.lower())
+
+import gensim
+from gensim.utils import simple_preprocess
+import nltk
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'summary', ])
+stop_words.extend(STOPWORDS)
+stop_words = list(set(stop_words))
+
+def sent_to_words(sentences):
+    for sentence in sentences:
+        # deacc=True removes punctuations
+        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))
+def remove_stopwords(texts):
+    return [[word for word in simple_preprocess(str(doc)) 
+             if word not in stop_words] for doc in texts]
+data = papers.paper_text_processed.values.tolist()
+data_words = list(sent_to_words(data))
+# remove stop words
+data_words = remove_stopwords(data_words)
+data_words = [x for x in data_words if len(x) > 0]
+
+
+import gensim.corpora as corpora
+# Create Dictionary
+id2word = corpora.Dictionary(data_words)
+
+# Create Corpus
+texts = data_words
+
+# Remove rare and common tokens.
+from gensim.corpora import Dictionary
+
+# Create a dictionary representation of the documents.
+dictionary = Dictionary(texts)
+
+# Filter out words that occur less than 20 documents, or more than 50% of the documents.
+dictionary.filter_extremes(no_below=10, no_above=0.5)
+
+# Bag-of-words representation of the documents.
+corpus = [dictionary.doc2bow(doc) for doc in texts]
+
+print('Number of unique tokens: %d' % len(dictionary))
+print('Number of documents: %d' % len(corpus))
+
+# Train LDA model.
+from gensim.models import LdaModel
+
+# Set training parameters.
+num_topics = 20
+chunksize = 2000
+passes = 20
+iterations = 400
+eval_every = None  # Don't evaluate model perplexity, takes too much time.
+
+# Make an index to word dictionary.
+temp = dictionary[0]  # This is only to "load" the dictionary.
+id2word = dictionary.id2token
+
+model = LdaModel(
+    corpus=corpus,
+    id2word=id2word,
+    chunksize=chunksize,
+    alpha='auto',
+    eta='auto',
+    iterations=iterations,
+    num_topics=num_topics,
+    passes=passes,
+    eval_every=eval_every
+)
+
+
+top_topics = model.top_topics(corpus)
+
+# Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
+avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+print('Average topic coherence: %.4f.' % avg_topic_coherence)
+
+from pprint import pprint
+pprint(top_topics)
+
 
 # In[]
 # Post Frequency by day and site
@@ -228,6 +345,7 @@ for Index, Row in CleanDisc_Final.iterrows():
     Token_Dates = Token_Dates.append(Local_df)
 
 Token_Dates.to_csv('News_Tokens.csv')
+print("Post data processed.")
 
 # In[]
 
